7b64a21e68920fb5c5434f44d8f7052d13393118
DESKTOP-5L5JHTL\Hp <jaikishore1997@gmail.com>
2022-02-13 01:16:05 +0530

    Implemented Singleton for Player and Enemy

---
+++ Assets/Scripts/SingletonBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonBehaviour<T> : MonoBehaviour where T : SingletonBehaviour<T>
+{
+    private static T instance=null;
+    public static T Instance {
+        get{ return instance; }
+    }
+
+    private void Awake() {
+        if(instance == null){
+            instance = (T)this;
+            DontDestroyOnLoad(this);
+        }else{
+            Destroy(this);
+        }
+    }
+}
