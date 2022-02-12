0aa09caf6cdedcd0827216ae2b1e24cdbe4acfa1
DESKTOP-5L5JHTL\Hp <jaikishore1997@gmail.com>
2022-02-13 01:05:35 +0530

    Implemented Singlton for Player and Enemy

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : SingletonBehaviour<Player>
+{
+    public void display(){
+        Debug.Log("Hello World");
+    }
+}
