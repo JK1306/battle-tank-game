2cf4f1dec45e36906eb2a58bd57c3721cd41382c
DESKTOP-5L5JHTL\Hp <jaikishore1997@gmail.com>
2022-02-16 20:29:56 +0530

    MVC implementation

---
+++ Assets/Scripts/MVC/PlayerTank/TankController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController
+{
+    public TankModel tankModel;
+    public TankView tankView;
+    public TankController(TankView tankView, TankModel tankModel){
+        this.tankModel = tankModel;
+        this.tankView = GameObject.Instantiate<TankView>(tankView);
+    }
+}
---
+++ Assets/Scripts/MVC/PlayerTank/TankServices.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankServices : MonoBehaviour
+{
+    public TankView tankView;
+    public TankModel tankModel;
+    public TankController tankController;
+    void Start()
+    {
+        tankModel = new TankModel();
+        tankController = new TankController(tankView, tankModel);
+    }
+
+    void Update()
+    {
+        
+    }
+}
